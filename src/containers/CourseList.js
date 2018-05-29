e04a1b0e47adc1d7c198e134250214d4689a40d4
ViranchiPaliwal <viranchi.paliwal@gmail.com>
2018-05-28 22:52:37 -0700

    added course service

--- src/containers/CourseList.js
+++ src/containers/CourseList.js
@@ -1,23 +1,50 @@
 import React from 'react';
 import CourseRow from '../components/CourseRow'
+import CourseService from '../services/CourseService';
+
 class CourseList extends React.Component {
     constructor() {
         super();
+        this.courseService = CourseService.instance;
+        this.state = {courses: []};
+
     }
+
+    renderCustomerRow(){
+        let courses = this.state.courses.map(
+            function (course) {
+                return <CourseRow key={course.id} course={course}/>
+            }
+        )
+        return (
+            courses
+        )
+    }
+
     render() {
         return (
             <div>
                 <h2>Course List</h2>
                 <table className='table'>
-                    <thead><tr><th>Title</th></tr></thead>
+                    <thead>
+                    <tr>
+                        <th>Title</th>
+                    </tr>
+                    </thead>
                     <tbody>
-                    <CourseRow/>
-                    <CourseRow/>
-                    <CourseRow/>
+                    {this.renderCustomerRow()}
                     </tbody>
                 </table>
             </div>
         )
+    }
+
+    componentDidMount() {
+        this.courseService
+            .findAllCourses()
+            .then((courses) => {
+                this.setState({courses: courses});
+            });
     }
 }
 export default CourseList;
