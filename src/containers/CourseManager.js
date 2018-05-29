189ce16ec20fcb045d8fefe5b7f7ce2b6c2f585f
ViranchiPaliwal <viranchi.paliwal@gmail.com>
2018-05-28 21:53:26 -0700

    added CourseList

--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
+import CourseList from './CourseList';
+
 export default class CourseManager extends Component{
     render(){
         return (
             <div className='container-fluid'>
             <h1>Course Manager</h1>
+            <CourseList/>
             </div>
         )
     }
