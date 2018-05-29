5bb48cef6144e5d5f7eff39a7938c872404f0b50
ViranchiPaliwal <viranchi.paliwal@gmail.com>
2018-05-28 21:50:53 -0700

    added CourseManager

---
+++ src/containers/CourseManager.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+export default class CourseManager extends Component{
+    render(){
+        return (
+            <div className='container-fluid'>
+            <h1>Course Manager</h1>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+
+import CourseManger from './containers/CourseManager';
 
 ReactDOM.render(
-    <h1>Course Manager</h1>,
+    <CourseManger/>,
     document.getElementById('root')
 );
