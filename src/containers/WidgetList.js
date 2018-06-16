85940b3f29b07331b2534bc467ad7121c81a4c1f
ViranchiPaliwal <viranchi.paliwal@gmail.com>
2018-06-16 11:45:38 -0700

    added functionality of adding a widget

--- src/containers/WidgetList.js
+++ src/containers/WidgetList.js
@@ -3,23 +3,45 @@ import ReactDOM from 'react-dom'
 import {Provider, connect} from 'react-redux'
 import {createStore} from 'redux'
 
-const WidgetList = ({widgets}) => (
+const Widget = ({widget}) => (
+    <li key={widget.id}>{widget.text}
+        <button onClick={}></button></li>
+)
+
+const WidgetList = ({widgets, dispatch}) => (
     <div>
         <h1>Widget List  {widgets.length}</h1>
+        <ul>
+            {widgets.map(widget => (<Widget widget={widget}/>))}
+        </ul>
+        <button onClick={e => (
+            dispatch({type: 'ADD_WIDGET'})
+        )}>Add widget</button>
     </div>
 )
 let initialState = {
     widgets: [
         {id: 0, text: "Widget 1"},
-        {id: 0, text: "Widget 2"},
-        {id: 0, text: "Widget 3"},
-        {id: 0, text: "Widget 4"},
-        {id: 0, text: "Widget 5"}
+        {id: 1, text: "Widget 2"},
+        {id: 2, text: "Widget 3"},
+        {id: 3, text: "Widget 4"},
+        {id: 4, text: "Widget 5"}
     ]
 }
 
-const widgetReducer = () =>{
-    return initialState
+const widgetReducer = (state = initialState, action) =>{
+    switch (action.type){
+
+        case 'ADD_WIDGET':
+            return {
+                widgets:[
+                    ...state.widgets,
+                    {id: 99, text: 'New Widget'}
+                ]
+            }
+        default:
+            return state
+    }
 }
 
 let store = createStore(widgetReducer)
@@ -35,3 +57,4 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root')
 )
+
